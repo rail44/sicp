91319fd870119d0febf08cc88efbabb66d9f41da
Satoshi Amemiya <rail.sky@gmail.com>
2015-01-10 23:17:04 +0900

    To ex-1.13

--- chapter_1.scm
+++ chapter_1.scm
@@ -33,3 +33,45 @@
 
 (define (my-sqrt x)
   (sqrt-iter 1.0 x))
+
+(define (cbrt x)
+  (define (improve guess)
+    (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+
+  (define (good-enough? guess)
+    (< (abs (- (improve guess) guess)) (abs (* x 0.00000001))))
+
+  (define (iter guess)
+    (if (good-enough? guess)
+      guess
+      (iter (improve guess))))
+
+  (iter 1))
+
+(define (A x y)
+  (cond ((= y 0) 0)
+	((= x 0) (* 2 y))
+	((= y 1 ) 2)
+	(else (A (- x 1)
+		 (A x (- y 1))))))
+
+(define (ex-1-11-rec n)
+  (if (< n 3)
+    n
+    (+ (ex-1-11-rec (- n 1)) (* 2 (ex-1-11-rec (- n 2))) (* 3 (ex-1-11-rec (- n 3))))))
+
+(define (ex-1-11-iter n)
+  (define (iter a b c count)
+    (if (= count 0)
+      a
+      (iter (+ a (* 2 b) (* 3 c)) a b (- count 1))))
+
+  (if (< n 3)
+    n
+    (iter 2 1 0 (- n 2))))
+
+(define (pascal l n)
+  (if (= l 1)
+    (if (= n 1) 1 0)
+    (+ (pascal (- l 1) (- n 1)) (pascal (- l 1) n))))
+
