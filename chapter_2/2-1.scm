2dfbe803e157885292afa05e757fa39877b505b6
Satoshi Amemiya <rail.sky@gmail.com>
2015-03-29 19:23:34 +0900

    Impl ex-2-3

--- chapter_2/2-1.scm
+++ chapter_2/2-1.scm
@@ -71,3 +71,56 @@
                          (x-point end-p))
                 (average (y-point start-p)
                          (y-point end-p)))))
+
+(define (make-rectangle p1 p2)
+  (cons p1 p2))
+
+(define (p1-rectangle r)
+  (car r))
+
+(define (p2-rectangle r)
+  (cdr r))
+
+(define (width-rectangle r)
+  (abs (- (x-point (p1-rectangle r))
+          (x-point (p2-rectangle r)))))
+
+(define (height-rectangle r)
+  (abs (- (y-point (p1-rectangle r))
+          (y-point (p2-rectangle r)))))
+
+(define (perimeter-rectangle r)
+  (+ (* (width-rectangle r) 2)
+     (* (height-rectangle r) 2)))
+
+(define (area-rectangle r)
+  (* (width-rectangle r) (height-rectangle r)))
+
+(define (test-rectangle)
+  (let ((p1 (make-point 1 3))
+        (p2 (make-point 3 -1)))
+    (let ((r (make-rectangle p1 p2)))
+      (assert
+        (= (area-rectangle r)
+           8))
+      (assert
+        (= (perimeter-rectangle r)
+           12)))))
+
+(define (make-rectangle-by-size base width height)
+  (cons base
+        (make-point
+          (+ (x-point base) width)
+          (+ (y-point base) height))))
+
+(define (test-rectangle-by-size)
+  (let ((base (make-point 1 3))
+        (w 5)
+        (h 12))
+    (let ((r (make-rectangle-by-size base w h)))
+      (assert
+        (= (area-rectangle r)
+           60))
+      (assert
+        (= (perimeter-rectangle r)
+           34)))))
