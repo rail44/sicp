7fdf3574a03154dbca730d29df04b678b56ec86a
Satoshi Amemiya <rail.sky@gmail.com>
2015-04-02 23:56:37 +0900

    Impl ex-2-8

--- chapter_2/2-1.scm
+++ chapter_2/2-1.scm
@@ -196,3 +196,7 @@
 
 (define (upper-bound i) (cdr i))
 (define (lower-bound i) (car i))
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
