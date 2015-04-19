3b954d299c9d400dfd8b157ccd89bf04e4fc117a
Satoshi Amemiya <rail.sky@gmail.com>
2015-04-19 18:11:35 +0900

    Impl ex-2-19

--- chapter_2/2-2.scm
+++ chapter_2/2-2.scm
@@ -48,3 +48,22 @@
       result
       (iter (cdr l) (cons (car l) result))))
   (iter l '()))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+          (+ (cc amount
+                 (except-first-denomination coin-values))
+             (cc (- amount
+                    (first-denomination coin-values))
+                 coin-values)))))
+
+(define (first-denomination coin-values)
+  (car coin-values))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+
+(define (no-more? coin-values)
+  (null? coin-values))
