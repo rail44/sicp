4b1494789c6c330ca86c2eca86bd0335230e0c93
Satoshi Amemiya <rail.sky@gmail.com>
2015-04-19 20:13:27 +0900

    Impl ex-2-24

--- chapter_2/2-2.scm
+++ chapter_2/2-2.scm
@@ -99,3 +99,9 @@
   (if (null? items)
     #t
     (my-for-each proc (cdr items))))
+
+(define (count-leaves x)
+  (cond ((null? x) 0)
+        ((not (pair? x)) 1)
+        (else  (+ (count-leaves (car x))
+                  (count-leaves (cdr x))))))
