106fd4245b8063ce1d03a541a5a4e6f2985c9e63
Satoshi Amemiya <rail.sky@gmail.com>
2015-04-19 14:57:07 +0900

    Impl ex-2-11

--- chapter_2/2-1.scm
+++ chapter_2/2-1.scm
@@ -186,12 +186,24 @@
                  (+ (upper-bound x) (upper-bound y))))
 
 (define (mul-interval x y)
-  (let ((p1 (* (lower-bound x) (lower-bound y)))
-        (p2 (* (lower-bound x) (upper-bound y)))
-        (p3 (* (upper-bound x) (lower-bound y)))
-        (p4 (* (upper-bound x) (upper-bound y))))
-    (make-interval (min p1 p2 p3 p4)
-                   (max p1 p2 p3 p4))))
+  (let ((c1 (< (* (upper-bound x) (lower-bound x)) 0))
+        (c2 (< (* (upper-bound y) (lower-bound y)) 0)))
+    (cond ((and c1 c2)
+           (let ((p1 (* (lower-bound x) (lower-bound y)))
+                 (p2 (* (lower-bound x) (upper-bound y)))
+                 (p3 (* (upper-bound x) (lower-bound y)))
+                 (p4 (* (upper-bound x) (upper-bound y))))
+             (make-interval (min p1 p4)
+                            (max p2 p3))))
+          (c1
+            (make-interval (* (lower-bound x) (upper-bound y))
+                           (* (upper-bound x) (upper-bound y))))
+          (c2
+            (make-interval (* (upper-bound x) (lower-bound y))
+                           (* (upper-bound x) (upper-bound y))))
+          (else
+            (make-interval (* (lower-bound x) (lower-bound y))
+                           (* (upper-bound x) (upper-bound y)))))))
 
 (define (div-interval x y)
   (if (< (* (upper-bound y) (lower-bound y)) 0)
