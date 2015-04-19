799fd12a3f7493d63f589f9dd49e98d3c6728a06
Satoshi Amemiya <rail.sky@gmail.com>
2015-04-19 20:35:28 +0900

    Fix square-list1

--- chapter_2/2-2.scm
+++ chapter_2/2-2.scm
@@ -90,7 +90,7 @@
 (define (square-list1 items)
   (if (null? items)
     '()
-    (cons (square (car items)) (square-list (cdr items)))))
+    (cons (square (car items)) (square-list1 (cdr items)))))
 
 (define (square-list2 items)
   (map square items))
