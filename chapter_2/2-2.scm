6329bef7af6696bd043e502bbcd45bff35017307
Satoshi Amemiya <rail.sky@gmail.com>
2015-04-19 17:51:42 +0900

    Read to ex-2-17

---
+++ chapter_2/2-2.scm
@@ -0,0 +1,36 @@
+(define (list-ref items n)
+  (if (= n 0)
+    (car items)
+    (list-ref (cdr items) (- n 1))))
+
+(define (my-length items)
+  (if (null? items)
+    0
+    (+ 1 (my-length (cdr items)))))
+
+(define (my-length2 items)
+  (define (length-iter a count)
+    (if (null? a)
+      count
+      (length-iter (cdr a) (+ 1 count))))
+  (length-iter items 0))
+
+(define (test-my-length)
+  (define odds (list 1 3 5 7))
+  (assert
+    (=
+      (length odds)
+      (my-length odds)
+      (my-length2 odds))))
+
+(define (my-append list1 list2)
+  (if (null? list1)
+    list2
+    (cons (car list1) (append (cdr list1) list2))))
+
+(define (test-my-append)
+  (define odds (list 1 3 5 7))
+  (assert
+    (equal?
+      (append odds odds)
+      (my-append odds odds))))
