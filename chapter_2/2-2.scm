cec37fbcd80bacf950ca12fdfce11b103767d301
Satoshi Amemiya <rail.sky@gmail.com>
2015-09-03 19:52:47 +0900

    ex-2-37

--- chapter_2/2-2.scm
+++ chapter_2/2-2.scm
@@ -301,3 +301,37 @@
     (equal?
       (accumulate-n + 0 l)
       (list 22 26 30))))
+
+(define m1
+  (list
+    (list 1 2 3 4)
+    (list 4 5 6 6)
+    (list 6 7 8 9)
+    (list 9 8 6 2)))
+
+(define m2
+  (list
+    (list 1 2 3 4)
+    (list 4 5 6 6)
+    (list 6 7 8 9)
+    (list 1 2 3 4)))
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (w)
+         (dot-product v w))
+       m))
+
+(define (transpose mat)
+  (accumulate-n (lambda (v result)
+                  (cons v result))
+                '()
+                mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (v)
+           (matrix-*-vector cols v))
+         m)))
