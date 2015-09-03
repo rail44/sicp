19d874c3842212267047e02b3fa5c1bd84764282
Satoshi Amemiya <rail.sky@gmail.com>
2015-09-03 19:07:16 +0900

    ex-2-36

--- chapter_2/2-2.scm
+++ chapter_2/2-2.scm
@@ -283,3 +283,21 @@
 
 (define (test-count-leaves-accum)
   (count-leaves-accum (list 1 (list 2 3 (list 5)) 6)))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+             '()
+             (cons (accumulate op init (map (lambda (x) (car x)) seqs))
+                   (accumulate-n op init (map (lambda (x) (cdr x)) seqs)))))
+
+(define (test-accumulate-n)
+  (define l
+    (list
+      (list 1 2 3)
+      (list 4 5 6)
+      (list 7 8 9)
+      (list 10 11 12)))
+  (assert
+    (equal?
+      (accumulate-n + 0 l)
+      (list 22 26 30))))
