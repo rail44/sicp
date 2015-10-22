c202e568d25e4005d7cd3b640e713115f1429084
Satoshi Amemiya <rail.sky@gmail.com>
2015-10-22 20:19:18 +0900

    Impl ex-2-62

--- chapter_2/2-3.scm
+++ chapter_2/2-3.scm
@@ -280,3 +280,22 @@
   (assert-equal
     (list 1 2 3 4 5)
     (adjoin-ordered-set 3 (list 1 2 3 4 5))))
+
+(define (union-ordered-set set1 set2)
+  (cond
+    ((null? set1) set2)
+    ((null? set2) set1)
+    (else
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1 (union-ordered-set (cdr set1) (cdr set2))))
+              ((< x1 x2)
+               (cons x1 (union-ordered-set (cdr set1) set2)))
+              (else
+                (cons x2 (union-ordered-set set1 (cdr set2)))))))))
+
+(define (test-union-ordered-set)
+  (assert-equal
+    (list 1 2 3 4 5)
+    (union-ordered-set (list 1 3 5) (list 2 3 4))))
+
