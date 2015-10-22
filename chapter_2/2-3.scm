123b7e15aa22ae5ffe00141efb6c980eb37eb1ae
Satoshi Amemiya <rail.sky@gmail.com>
2015-10-22 15:32:24 +0900

    Impl ex-2-53

--- chapter_2/2-3.scm
+++ chapter_2/2-3.scm
@@ -9,3 +9,45 @@
     (equal?
       '(apple pear)
       (my-memq 'apple '(x (apple sauce) y apple pear)))))
+
+(define (ex-2-53)
+  (display '(list 'a 'b 'c))
+  (newline)
+  (display (list 'a 'b 'c))
+  (newline)
+
+  (newline)
+  (display '(list (list 'george)))
+  (newline)
+  (display (list (list 'george)))
+  (newline)
+
+  (newline)
+  (display '(cdr '((x1 x2) (y1 y2))))
+  (newline)
+  (display (cdr '((x1 x2) (y1 y2))))
+  (newline)
+
+  (newline)
+  (display '(cadr '((x1 x2) (y1 y2))))
+  (newline)
+  (display (cadr '((x1 x2) (y1 y2))))
+  (newline)
+
+  (newline)
+  (display '(pair? (car '(a short list))))
+  (newline)
+  (display (pair? (car '(a short list))))
+  (newline)
+
+  (newline)
+  (display '(my-memq 'red '((red shoes) (blue socks))))
+  (newline)
+  (display (my-memq 'red '((red shoes) (blue socks))))
+  (newline)
+
+  (newline)
+  (display '(my-memq 'red '(red shoes blue socks)))
+  (newline)
+  (display (my-memq 'red '(red shoes blue socks)))
+  (newline))
