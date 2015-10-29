b4e487d78a83cb789199a4dcb0d6ebb18fed38da
Satoshi Amemiya <rail.sky@gmail.com>
2015-10-29 18:16:29 +0900

    Add comment for ex-2-63

--- chapter_2/2-3.scm
+++ chapter_2/2-3.scm
@@ -331,16 +331,24 @@
 (define (tree->list-1 tree)
   (if (null? tree)
     '()
-    (append (tree->list-1 (left-branch tree))
+    (append (tree->list-1 (my-left-branch tree))
             (cons (entry tree)
-                  (tree->list-1 (right-branch tree))))))
+                  (tree->list-1 (my-right-branch tree))))))
 
 (define (tree->list-2 tree)
   (define (copy-to-list tree result-list)
     (if (null? tree)
       result-list
-      (copy-to-list (left-branch tree)
+      (copy-to-list (my-left-branch tree)
                     (cons (entry tree)
-                          (copy-to-list (right-branch tree)
+                          (copy-to-list (my-right-branch tree)
                                         result-list)))))
   (copy-to-list tree '()))
+
+; ex-2-63-a
+; tree->list-1, tree->list-2とも要素を昇順に並べた同一のリストを作る。
+
+; ex-2-63-b
+; tree->list-2はO(n)で増加するが、tree->list-1はapendを使っているためO(nlogn)で増加する
+; ↑の認識であってるかどうか検索したら、反復的プロセスか再帰的プロセスかに言及しているものがあったが、どちらもステップ数の増加という観点でいうと同じであるように思える(たぶん)
+; tree->list-1, tree->list-2とも要素を昇順に並べた同一のリストを作る。
