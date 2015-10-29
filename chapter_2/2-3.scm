15acf9829b18bff188b3c08474c673d73e0d9e99
Satoshi Amemiya <rail.sky@gmail.com>
2015-10-29 17:59:07 +0900

    Impl ex-2-63

--- chapter_2/2-3.scm
+++ chapter_2/2-3.scm
@@ -299,3 +299,48 @@
     (list 1 2 3 4 5)
     (union-ordered-set (list 1 3 5) (list 2 3 4))))
 
+(define (entry tree) (car tree))
+
+(define (my-left-branch tree) (cadr tree))
+
+(define (my-right-branch tree) (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-tree-set? x set)
+  (cond ((null? set) #f)
+        ((= x (entry set)) #t)
+        ((< x (entry set))
+         (element-of-tree-set? x (my-left-branch set)))
+        ((> x (entry set))
+         (element-of-tree-set? x (my-right-branch set)))))
+
+(define (adjoin-tree-set x set)
+  (cond ((null? set) make-tree x '() '())
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set)
+                    (adjoin-tree-set x (my-left-branch set))
+                    (my-right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (my-left-branch set)
+                    (adjoin-tree-set x (my-right-branch set))))))
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+    '()
+    (append (tree->list-1 (left-branch tree))
+            (cons (entry tree)
+                  (tree->list-1 (right-branch ree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree rseult-list)
+    (if (null? tree)
+      result-list
+      (copy-to-list (left-branch tree)
+                    (cons (entry tree)
+                          (copy-to-list (right-branch tree)
+                                        result-list)))))
+  (copy-to-list tree 1()))
